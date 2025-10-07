83fcac64bc4ab7c5651a6907be61bb46777fae71
RasaraThathsarana <rasarathathsarana63@gmail.com>
2025-10-07 09:33:47 +0530

    vit

--- models/classifier.py
+++ models/classifier.py
@@ -1,89 +1,97 @@
 import torch
 from models.spatialtransform import spatial_transformer
 from models.net import ResNet
+from models.vit import ViTClassifier
 
 
-def ClassifierNetWork(config,feat_ch):
-    if config.detector_backbone == 'CustomResNet':
-        return CustomClassifier(config,feat_ch)
-    elif config.detector_backbone == 'ResNet34':
-        return ResnetClassifier(config,feat_ch)
+def ClassifierNetWork(config, feat_ch):
+    if config.detector_backbone == "CustomResNet":
+        return CustomClassifier(config, feat_ch)
+    elif config.detector_backbone == "ResNet34":
+        return ResnetClassifier(config, feat_ch)
+    elif config.detector_backbone == "ViT":
+        return ViTClassifier(config, feat_ch)
     else:
-        raise Exception('invalid backbone')
+        raise Exception("invalid backbone")
 
-class ResnetClassifier (torch.nn.Module):
-    
-    def __init__(self, config,feat_ch):
+
+class ResnetClassifier(torch.nn.Module):
+
+    def __init__(self, config, feat_ch):
         super(ResnetClassifier, self).__init__()
-
 
         self.psize = (config.patch_size, config.patch_size)
         self.cnn = ResNet(256, 512, 3, use_padding=True)
         self.fcn = torch.nn.Sequential(
-            torch.nn.Linear(512,config.num_classes),
-            torch.nn.Softmax(dim=-1))
-        self.global_avg_pool = torch.nn.AvgPool2d((self.psize[0],self.psize[1]))
-    
+            torch.nn.Linear(512, config.num_classes), torch.nn.Softmax(dim=-1)
+        )
+        self.global_avg_pool = torch.nn.AvgPool2d((self.psize[0], self.psize[1]))
+
     def forward(self, featuremap, bboxes, stride=None):
         diag = {}
 
         B, N, _ = bboxes.shape
         _, C, _, _ = featuremap.shape
 
-        patches = spatial_transformer(featuremap, bboxes, self.psize).view(-1, C, *self.psize)
+        patches = spatial_transformer(featuremap, bboxes, self.psize).view(
+            -1, C, *self.psize
+        )
         patches = self.cnn(patches)
-        features = self.global_avg_pool(patches).view(patches.shape[0],-1)
+        features = self.global_avg_pool(patches).view(patches.shape[0], -1)
         logits = self.fcn(features)
-        logits = logits.view(B, N,-1)
-        labels = torch.argmax(logits,dim=2)
+        logits = logits.view(B, N, -1)
+        labels = torch.argmax(logits, dim=2)
 
         # get labels
-        labels = torch.argmax(logits,dim=2)
-        diag['patches'] = patches
+        labels = torch.argmax(logits, dim=2)
+        diag["patches"] = patches
 
         return labels, logits, diag
 
 
-
 class CustomClassifier(torch.nn.Module):
-    
-    def __init__(self, config,feat_ch):
+
+    def __init__(self, config, feat_ch):
         super(CustomClassifier, self).__init__()
         self.encoder = Encoder(config)
         self.classifier = Classifier(config.num_classes, self.encoder.out_channels)
         self.psize = (config.patch_size, config.patch_size)
-    
+
     def forward(self, featuremap, bboxes, stride=None):
         diag = {}
 
         B, N, _ = bboxes.shape
         _, C, _, _ = featuremap.shape
 
-        # extract patch 
-        patches = spatial_transformer(featuremap, bboxes, self.psize).view(-1, C, *self.psize)
-        
+        # extract patch
+        patches = spatial_transformer(featuremap, bboxes, self.psize).view(
+            -1, C, *self.psize
+        )
+
         # compute logits
         latent = self.encoder(patches)
         logits = self.classifier(latent)
-        logits = logits.view(B,N,-1)
+        logits = logits.view(B, N, -1)
 
         # get labels
-        labels = torch.argmax(logits,dim=2)
+        labels = torch.argmax(logits, dim=2)
 
-        diag['patches'] = patches
+        diag["patches"] = patches
 
         return labels, logits, diag
 
+
 class Classifier(torch.nn.Module):
-    def __init__(self, number_class,in_channels):
+    def __init__(self, number_class, in_channels):
         super(Classifier, self).__init__()
         self.dense = torch.nn.Linear(in_channels, number_class)
         self.softmax = torch.nn.Softmax(dim=-1)
-    
+
     def forward(self, x):
         x = self.dense(x)
         x = self.softmax(x)
         return x
+
 
 class Encoder(torch.nn.Module):
     def __init__(self, config):
@@ -98,52 +106,53 @@ class Encoder(torch.nn.Module):
         for i in range(num_levels):
             c_in = c_out
             c_out = num_channels * 2**i
-            self.layers += [ResNet(in_channels=c_in, num_channels=c_out, num_blocks=num_blocks)]
+            self.layers += [
+                ResNet(in_channels=c_in, num_channels=c_out, num_blocks=num_blocks)
+            ]
             self.layers += [torch.nn.MaxPool2d(2, stride=2)]
         self.out_channels = c_out
-        self.layers += [ResNet(in_channels=c_out, num_channels=c_out, num_blocks=num_blocks)]
+        self.layers += [
+            ResNet(in_channels=c_out, num_channels=c_out, num_blocks=num_blocks)
+        ]
         self.layers = torch.nn.Sequential(*self.layers)
-    
+
     def forward(self, x):
         x = self.layers(x)
         x = x.view(x.shape[0], x.shape[1])
         return x
 
-class ClassifierGAP (torch.nn.Module):
-    
-    def __init__(self, config,feat_ch):
+
+class ClassifierGAP(torch.nn.Module):
+
+    def __init__(self, config, feat_ch):
         super(ClassifierGAP, self).__init__()
 
         self.psize = (config.patch_size, config.patch_size)
         self.cnn = ResNet(feat_ch, 512, 3, use_padding=True)
         self.fcn = torch.nn.Sequential(
-            torch.nn.Linear(512,config.num_classes),
-            torch.nn.Softmax(dim=-1))
-        self.global_avg_pool = torch.nn.AvgPool2d((self.psize[0],self.psize[1]))
-    
+            torch.nn.Linear(512, config.num_classes), torch.nn.Softmax(dim=-1)
+        )
+        self.global_avg_pool = torch.nn.AvgPool2d((self.psize[0], self.psize[1]))
+
     def forward(self, featuremap, bboxes, stride=None):
         diag = {}
 
         B, N, _ = bboxes.shape
         _, C, _, _ = featuremap.shape
 
-        # extract patch 
-        patches = spatial_transformer(featuremap, bboxes, self.psize).view(-1, C, *self.psize)
-        
+        # extract patch
+        patches = spatial_transformer(featuremap, bboxes, self.psize).view(
+            -1, C, *self.psize
+        )
+
         # compute logits
         patches = self.cnn(patches)
-        features = self.global_avg_pool(patches).view(patches.shape[0],-1)
+        features = self.global_avg_pool(patches).view(patches.shape[0], -1)
         logits = self.fcn(features)
 
         # get labels
-        labels = torch.argmax(logits,dim=2)
-        diag['patches'] = patches
-        diag['features'] = features
+        labels = torch.argmax(logits, dim=2)
+        diag["patches"] = patches
+        diag["features"] = features
 
         return labels, logits, diag
-
-
-
-
-
-
---
+++ models/vit.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn.functional as F
+from torchvision.models.vision_transformer import vit_b_16, ViT_B_16_Weights
+from models.spatialtransform import spatial_transformer
+
+
+class ViTClassifier(torch.nn.Module):
+    def __init__(self, config, feat_ch):
+        super(ViTClassifier, self).__init__()
+        self.psize = (config.patch_size, config.patch_size)
+
+        # Load pretrained ViT backbone (or custom if you prefer)
+        self.vit = vit_b_16(weights=ViT_B_16_Weights.IMAGENET1K_V1)
+        self.vit.heads = torch.nn.Identity()  # remove classification head
+
+        # Optional: freeze ViT if using pretrained features
+        # for p in self.vit.parameters():
+        #     p.requires_grad = False
+
+        self.classifier = torch.nn.Linear(768, config.num_classes)
+
+    def forward(self, featuremap, bboxes, stride=None):
+        B, N, _ = bboxes.shape
+        _, C, _, _ = featuremap.shape
+
+        patches = spatial_transformer(featuremap, bboxes, self.psize).view(
+            -1, C, *self.psize
+        )
+        patches = F.interpolate(
+            patches, size=(224, 224), mode="bilinear", align_corners=False
+        )
+
+        features = self.vit(patches)  # [B*N, 768]
+        logits = self.classifier(features)  # [B*N, num_classes]
+        logits = logits.view(B, N, -1)
+        labels = torch.argmax(logits, dim=2)
+
+        return labels, logits, {"patches": patches, "features": features}
